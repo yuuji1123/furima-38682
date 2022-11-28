701e9e7bf46203c717dbae240c991c523658054b
yuuji1123 <wagnas.syoukankyou@gmail.com>
2022-11-28 16:07:09 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :family_name, presence: true
-         validates :first_name, presence: true
-         validates :family_name_kana, presence: true
-         validates :first_name_kana, presence: true
+         PASSWORD_REGEX =  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  with_options presence: true do
+        validates :nickname
+        validates :password, format: {with: PASSWORD_REGEX}, allow_blank: true
+        with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "is invalid. Input full-width characters."} do
+         validates :family_name
+         validates :first_name
+        end
+        with_options format: {with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} do
+         validates :family_name_kana
+         validates :first_name_kana
+        end
+        validates :birthday
+        end
 end

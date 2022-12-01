def0580feec23f0ea5bb79525ec0619f2ffcc6d8
yuuji1123 <wagnas.syoukankyou@gmail.com>
2022-12-01 11:27:48 +0900

    itemsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,15 @@
 ActiveRecord::Schema.define(version: 2022_11_25_070646) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "item_name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_fee_id", null: false
+    t.integer "ship_address_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.integer "price", null: false
+    t.integer "user", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

524a73e08a8bc4999924f22078a59fc3bfa9667d
yuuji1123 <wagnas.syoukankyou@gmail.com>
2022-12-08 16:23:58 +0900

    adressのバリデーション記述

--- app/models/adress.rb
+++ app/models/adress.rb
@@ -1,2 +1,12 @@
 class Adress < ApplicationRecord
+  belongs_to :record
+
+
+  validates :postcode, presence: true
+  validates :city, presence: true
+  validates :block, presence: true
+  validates :phone_number, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :ship_address
 end
